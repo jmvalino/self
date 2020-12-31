0ac837c53c8184e3efe28125463e5b144e56f09d
jmvalino <>
2020-12-31 09:43:48 +0800

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,34 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 import {
   GithubIcon,
   LinkedInIcon,
   SunIcon,
   MoonIcon,
+  MailIcon,
 } from "../components/icons";
 
 const Home: React.FC = () => {
+  const [theme, setTheme] = useState("");
+  useEffect(() => {
+    console.log(theme, "Theme");
+  }, [theme]);
+
   return (
-    <div className="flex h-screen flex-col md:flex-row">
-      <nav className="flex flex-col p-5 border-r md:w-80">
-        <div className="flex flex-col bg-primary-900 rounded-full w-14 h-14 items-center justify-center">
-          <div className="text-white font-semibold text-xl">JM</div>
+    <div className={`${theme} flex h-screen flex-col md:flex-row`}>
+      <nav className="flex flex-col p-5 border-r md:w-80 dark:bg-gray-900 dark:border-transparent">
+        <button onClick={() => setTheme("dark")}>
+          <div className="flex flex-col bg-primary-900 rounded-full w-14 h-14 items-center justify-center">
+            <div className="text-white font-semibold text-xl ">JM</div>
+          </div>
+        </button>
+
+        <div className="text-2xl  font-semibold my-4 dark:text-white">
+          Jose Mari Valino
         </div>
-        <div className="text-2xl  font-semibold my-4">Jose Mari Valino</div>
         <p className="text-primary-100 leading-5 mb-14">
-          Curabitur felis nunc, vehicula sed interdum feugiat, porttitor nec
-          tortor. Aenean vel justo quam. Nunc lobortis, urna at tincidunt
-          malesuada, ipsum risus finibus tellus, a commodo nulla erat at nibh.
+          IoT Solutions Architect , loves prototyping and play latest tech in
+          web.
         </p>
 
         <div className="flex flex-wrap gap-3">
@@ -34,18 +44,26 @@ const Home: React.FC = () => {
         </button>
 
         <div className="flex gap-3">
-          <GithubIcon />
-          <LinkedInIcon />
+          <a href="https://github.com/jmvalino">
+            <GithubIcon />
+          </a>
+          <a href="https://linkedin.com/in/jmvalino" target="blank">
+            <LinkedInIcon />
+          </a>
+
+          <MailIcon />
         </div>
 
-        <div className="mt-auto w-20 flex flex-col items-center gap-1">
+        {/* <div className="mt-auto w-20 flex flex-col items-center gap-1">
           <SunIcon />
           <span className="h-12 rounded-full w-1 bg-gray-400" />
           <MoonIcon />
-        </div>
+        </div> */}
       </nav>
-      <main className="flex-grow p-3">
-        <h2>main content goes here..</h2>
+      <main className="flex-grow p-3 dark:bg-black">
+        <button>
+          <h2></h2>
+        </button>
       </main>
     </div>
   );
